6f8fc056e381d963f4db7a57d7a21a1df9c9c028
RodolpheBeloncle <rbeloncle@gmail.com>
2021-10-17 22:25:55 +0200

    fetch data countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Countries = () => {
+    const [data,setData] = useState([]);
+
+    // fetch Data from Api request countries
+    useEffect(()=>{
+        axios
+        .get('https://restcountries.com/v3.1/all')
+        .then((res) => setData(res.data.flatMap(element => 
+        {return [element.capital,element.flag]})));
+        console.log(data)
+    },[]);
+    
+
     return (
         <div>
+        <p>{data}</p>
+        <button >Say Aurevoirt</button>
             
         </div>
     );
